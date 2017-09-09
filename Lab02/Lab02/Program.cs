71e7fc3bf943a76f220aa611b1a3967893cdd4cf
poohpaintii <poohpaintii@gmail.com>
2017-09-09 18:14:31 +0700

    E-W5P1(2-03)

--- Lab02/Lab02/Program.cs
+++ Lab02/Lab02/Program.cs
@@ -3,10 +3,25 @@
 
 namespace Lab02
 {
-    public class GotoTest
+    public class TryCatch
     {
         static void Main(string[] args)
         {
+            int a = 0;
+            try
+            {
+
+                Console.WriteLine(100 / a);
+            }
+            catch (NullReferenceException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
+
 }
