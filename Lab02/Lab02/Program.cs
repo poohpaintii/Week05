c3931a0dbc1c897401ba4f7f7a55429aeaf58bd2
poohpaintii <poohpaintii@gmail.com>
2017-09-09 18:00:49 +0700

    E-W5P1(2-02)

--- Lab02/Lab02/Program.cs
+++ Lab02/Lab02/Program.cs
@@ -3,10 +3,21 @@
 
 namespace Lab02
 {
-    public class GotoTest
+    public class TryCatch
     {
         static void Main(string[] args)
         {
+            object o2 = null;
+            try
+            {
+                int i2 = (int)o2;
+                Console.WriteLine("i2 = {0}", i2);
+            }
+            catch
+            {
+                Console.WriteLine("Error, null object assignment.");
+            }
         }
     }
+
 }
