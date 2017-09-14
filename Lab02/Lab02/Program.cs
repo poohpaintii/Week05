6af5c368bd543ab2abb6c1f29dbcc6012a79ae66
poohpaintii <poohpaintii@gmail.com>
2017-09-14 18:37:28 +0700

    การเปลี่ยนทิศทางแบบมีเงื่อนไข (Conditional Branching)

--- Lab02/Lab02/Program.cs
+++ Lab02/Lab02/Program.cs
@@ -1,12 +1,35 @@
 ﻿using System;
-
-
-namespace Lab02
+using System.IO;
+public class switchLearning
 {
-    public class GotoTest
+    public static void Main()
     {
-        static void Main(string[] args)
+        Console.Write("Input your grade (A, B, C, D or F) : ");
+        string gradeString = Console.ReadLine();
+        string message;
+        switch (gradeString.ToUpper())
         {
+            case "A":
+                message = "Excellent";
+                break;
+            case "B":
+                message = "Good";
+                break;
+            case "C":
+                message = "Cool";
+                break;
+            case "D":
+                message = "Try";
+                break;
+            case "F":
+                message = "Get out!!";
+                break;
+            default:
+                message = "Incorrect grade";
+                break;
         }
+        Console.WriteLine(message);
     }
 }
+
+
