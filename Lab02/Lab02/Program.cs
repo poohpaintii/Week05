a105c37d04f060e61b6544e88b1de09b720a875a
poohpaintii <poohpaintii@gmail.com>
2017-09-14 17:50:18 +0700

    ExceptionLearning

--- Lab02/Lab02/Program.cs
+++ Lab02/Lab02/Program.cs
@@ -1,12 +1,36 @@
 ﻿using System;
-
-
-namespace Lab02
+using System.IO;
+public class ExceptionLearning
 {
-    public class GotoTest
+    public static void Main()
     {
-        static void Main(string[] args)
+        int a = 10;
+        int b = 20;
+        int c;
+        try
         {
+            c = div(a, b);
         }
+        catch (DivideByZeroException e)
+        {
+
+            Console.WriteLine("DivideByZeroException");
+            Console.WriteLine(e.Message);
+        }
+        catch (NullReferenceException e)
+        {
+            Console.WriteLine("NullReferenceException");
+            Console.WriteLine(e.Message);
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Exception");
+            Console.WriteLine(e.Message);
+        }
+    }
+    private static int div(int a, int b)
+    {
+        throw new FormatException();
     }
 }
